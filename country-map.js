d6b8b4e62c8c1259333e8687665ebb437e3bfc31
Lisa Övermyr <lisaovermyr@gmail.com>
2014-08-07 12:20:30 -0400

    Add demonym to countrymap

--- country-map.js
+++ country-map.js
@@ -5,6 +5,7 @@ var countryMap = {
 
 require('world-countries').forEach(function (country) {
   countryMap[country.name.toLowerCase()] = country.name
+  countryMap[country.demonym.toLowerCase()] = country.name
   country.altSpellings.forEach(function (altSpelling) {
      if (altSpelling.length > 2)
     countryMap[altSpelling.toLowerCase()] = country.name
--- test.js
+++ test.js
@@ -2,12 +2,13 @@ var test = require('tap').test
   , countryCount = require('./country-count')
 
 test('should return list with countries and nr of occurances', function (t) {
-  var possibleCountries = ['USA', 'Sweden', 'United States of America', 'America', 'No country']
+  var possibleCountries = ['USA', 'Sweden', 'United States of America', 'America', 'No country', 'danish']
     , countryArr = countryCount(possibleCountries)
 
   t.deepEqual(countryArr, {
       'United States': 3
     , 'Sweden': 1
+    , 'Denmark': 1
   })
 
   t.end()
